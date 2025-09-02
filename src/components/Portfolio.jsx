f839b224c3e2a6de7221cda4e1fd591dc4ad2f9a
kshitizrajan7 <kshitizrajandev@gmail.com>
2025-09-02 10:19:55 +0545

    enabled all the components

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { useState } from "react";
 import { BsGrid } from "react-icons/bs";
 import { FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
@@ -11,7 +12,7 @@ const Portfolio = () => {
     <div className="relative w-screen h-screen flex flex-col justify-between">
       {/* Popover panel */}
       <div
-        className={`flex-1 mx-5 my-5 mb-27 border-2 border-blue-500 rounded-xl 
+        className={`flex-1 mx-5 my-5 mb-27 border border-blue-500/50 rounded-xl 
           bg-white/10 backdrop-blur-sm text-white p-4 
           transition-all duration-500 flex flex-col items-center gap-6 
           overflow-y-auto
@@ -23,7 +24,7 @@ const Portfolio = () => {
           <img
             src="/profile.jpg"
             alt="photo"
-            className="w-24 h-24 sm:w-32 sm:h-32 rounded-full object-cover"
+            className="w-24 h-24 sm:w-32 sm:h-32 rounded-full object-cover shadow-[0_0_15px_#ffffff]"
           />
 
           {/* Welcome Text */}
@@ -42,8 +43,8 @@ const Portfolio = () => {
         {/* Projects / Cards Section */}
         <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
           {/* My Projects*/}
-          <div className="relative rounded-lg p-4  hover:scale-105 transition-transform duration-400 cursor-pointer overflow-hidden
-                bg-[url('/projects.png')] bg-cover bg-center h-64 sm:h-72  hover:shadow-[0_0_20px_rgba(59,130,246,0.7)]">
+          <a href={'https://github.com/kshitizrajan7'} target="_blank" className="relative rounded-lg p-4 border border-blue-500/50 hover:scale-105 transition-transform duration-400 cursor-pointer overflow-hidden
+                bg-[url('/projects.png')] bg-cover bg-center h-64 sm:h-72  hover:shadow-[0_0_15px_#ffffff]">
             {/* Optional dark overlay for readability */}
             <div className="absolute inset-0 bg-black/50" />
             {/* Card content */}
@@ -55,10 +56,10 @@ const Portfolio = () => {
                 </p>
               </div>
             </div>
-          </div>
+          </a>
           {/* My Skills */}
-          <div className="relative rounded-lg p-4 hover:scale-105 transition-transform duration-400 cursor-pointer overflow-hidden
-                bg-[url('/skills.png')] bg-cover bg-center h-64 sm:h-72  hover:shadow-[0_0_20px_rgba(59,130,246,0.7)]">
+          <a href="https://www.linkedin.com/in/kshitiz-rajan-4b7a45344/" target="_blank" className="relative rounded-lg p-4 border border-blue-500/50 hover:scale-105 transition-transform duration-400 cursor-pointer overflow-hidden
+                bg-[url('/skills.png')] bg-cover bg-center h-64 sm:h-72  hover:shadow-[0_0_15px_#ffffff]">
             {/* Optional dark overlay for readability */}
             <div className="absolute inset-0 bg-black/50" />
             {/* Card content */}
@@ -70,10 +71,10 @@ const Portfolio = () => {
                 </p>
               </div>
             </div>
-          </div>
+          </a>
           {/* Latest Video */}
-          <div className="relative rounded-lg p-4 hover:scale-105 transition-transform duration-400 cursor-pointer overflow-hidden
-                bg-[url('/latest.png')] bg-cover bg-center h-64 sm:h-72  hover:shadow-[0_0_20px_rgba(59,130,246,0.7)]">
+          <a href="https://lnkd.in/eMYAs7E4" target="_blank" className="relative rounded-lg p-4 border border-blue-500/50 hover:scale-105 transition-transform duration-400 cursor-pointer overflow-hidden
+                bg-[url('/latest.png')] bg-cover bg-center h-64 sm:h-72  hover:shadow-[0_0_15px_#ffffff]">
             {/* Optional dark overlay for readability */}
             <div className="absolute inset-0 bg-black/50" />
             {/* Card content */}
@@ -85,7 +86,7 @@ const Portfolio = () => {
                 </p>
               </div>
             </div>
-          </div>
+          </a>
         </div>
 
         {/* Social Links */}
@@ -93,44 +94,44 @@ const Portfolio = () => {
           <p className="text-white/80">You can reach out or check out what I create on:</p>
           <div className="flex flex-wrap justify-center gap-4 mt-4">
             <a
-              href="https://github.com/YourUsername"
+              href="https://github.com/kshitizrajan7"
               target="_blank"
               className="bg-white/20 p-3 rounded-full hover:bg-blue-500 transition-colors"
             >
               <FaGithub size={24} />
             </a>
             <a
-              href="https://linkedin.com/in/YourUsername"
+              href="https://linkedin.com/in/kshitiz-rajan-4b7a45344"
               target="_blank"
-              className="bg-white/20 p-3 rounded-full hover:bg-blue-500 transition-colors"
+              className="bg-[#0A66C2] p-3 rounded-full hover:bg-blue-500 transition-colors"
             >
               <FaLinkedin size={24} />
             </a>
             <a
-              href="https://youtube.com/YourChannel"
+              href="https://youtube.com/@KshitizRajan7"
               target="_blank"
-              className="bg-white/20 p-3 rounded-full hover:bg-blue-500 transition-colors"
+              className="bg-[#FF0000] p-3 rounded-full hover:bg-blue-500 transition-colors"
             >
               <FaYoutube size={24} />
             </a>
             <a
-              href="mailto:your-email@gmail.com"
+              href="mailto:kshitizrajan1234@gmail.com"
               target="_blank"
-              className="bg-white/20 p-3 rounded-full hover:bg-blue-500 transition-colors"
+              className="bg-[#188038] p-3 rounded-full hover:bg-blue-500 transition-colors"
             >
               <MdEmail size={24} />
             </a>
             <a
-              href="https://facebook.com/YourProfile"
+              href="https://facebook.com/ksitija.rajana"
               target="_blank"
-              className="bg-white/20 p-3 rounded-full hover:bg-blue-500 transition-colors"
+              className="bg-[#1877F2] p-3 rounded-full hover:bg-blue-500 transition-colors"
             >
               <FaFacebook size={24} />
             </a>
             <a
-              href="https://instagram.com/YourProfile"
+              href="https://instagram.com/kshitiz_rajan"
               target="_blank"
-              className="bg-white/20 p-3 rounded-full hover:bg-blue-500 transition-colors"
+              className="bg-[#833AB4] p-3 rounded-full hover:bg-blue-500 transition-colors"
             >
               <FaInstagram size={24} />
             </a>
@@ -142,7 +143,7 @@ const Portfolio = () => {
       <div className="fixed bottom-5 left-1/2 transform -translate-x-1/2 z-50">
         <div
           className="flex items-center justify-center w-14 h-14 sm:w-16 sm:h-16 
-            border-2 border-blue-500 text-blue-500 rounded-full
+             shadow-[0_0_20px_rgba(59,130,246,0.7)] text-blue-500 rounded-full
             transition-all duration-300 hover:shadow-[0_0_15px_#ffffff] hover:scale-105 cursor-pointer"
           onClick={() => setOpen(!open)}
         >
