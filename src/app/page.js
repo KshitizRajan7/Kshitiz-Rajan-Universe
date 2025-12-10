eb192cc00c6a37d6a8cf9f8ee42558f506067abe
kshitizrajan7 <kshitizrajandev@gmail.com>
2025-12-10 17:52:22 +0545

    add spinning earth

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div className="relative h-screen w-screen overflow-hidden bg-black">
       {/* 3D Earth background */}
-      {/* <div className="absolute inset-0">
-        {/* <SpinningEarthScene /> 
-      </div> */}
+      <div className="absolute inset-0">
+        <SpinningEarthScene /> 
+      </div>
 
       {/* Overlay text */}
       <p className="text-white absolute top-1/2 left-1/2 
